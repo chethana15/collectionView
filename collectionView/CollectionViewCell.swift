2bc0abd1ea95a23b88c34c10f1073322178017c5
Chethana N P <chethana.np@cumulations.com>
2022-04-26 13:14:18 +0530

    collection view with images

---
+++ collectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  collectionView
+//
+//  Created by Cumulations Technologies Private Limited on 26/04/22.
+//
+
+import UIKit
+import Gemini
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- collectionView/ViewController.swift
+++ collectionView/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = ["cheese-burger", "desk", "flower", "help", "school", "studying"]
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
 
 
 }
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: images[indexPath.row])
+        cell.layer.cornerRadius = cell.frame.height / 2
+        return cell
+    }
+    
+    
+}
